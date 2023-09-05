4341bfdc51b6d7e2f56dee69c7617a603a2d3ea1
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-09-05 19:20:33 +0300

    Update ImageGallery.js

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -9,49 +9,51 @@ export const ImageGallery = ({ apiKey, searchQuery, page, onLoadMore, componentK
   const [totalImages, setTotalImages] = useState(0);
 
   useEffect(() => {
+    const fetchImages = async () => {
+      setLoading(true);
+
+      try {
+        const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
+          searchQuery
+        )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.totalHits === 0) {
+          console.log('No images found.');
+          return;
+        }
+
+        const newImages = data.hits.map((image) => ({
+          id: image.id,
+          webformatURL: image.webformatURL,
+          tags: image.tags,
+          likes: image.likes,
+          views: image.views,
+          comments: image.comments,
+          downloads: image.downloads,
+        }));
+
+        // Фільтруємо унікальні зображення і додаємо їх до попереднього списку
+        setImages((prevImages) => [...prevImages, ...newImages.filter(newImage => !prevImages.some(prevImage => prevImage.id === newImage.id))]);
+        setTotalImages(data.totalHits);
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     if (!searchQuery) return;
     fetchImages();
   }, [apiKey, searchQuery, page, componentKey]);
 
-  const fetchImages = async () => {
-    setLoading(true);
-
-    try {
-      const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-        searchQuery
-      )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
-
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data.totalHits === 0) {
-        console.log('No images found.');
-        return;
-      }
-
-      const newImages = data.hits.map((image) => ({
-        id: image.id,
-        webformatURL: image.webformatURL,
-        tags: image.tags,
-        likes: image.likes,
-        views: image.views,
-        comments: image.comments,
-        downloads: image.downloads,
-      }));
-
-      // Фільтруємо унікальні зображення і додаємо їх до попереднього списку
-      setImages((prevImages) => [...prevImages, ...newImages.filter(newImage => !prevImages.some(prevImage => prevImage.id === newImage.id))]);
-      setTotalImages(data.totalHits);
-    } catch (error) {
-      console.error('Error fetching images:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   const handleLoadMore = () => {
     onLoadMore(); // Виклик функції з батьківського компонента для збільшення сторінки
   };
+
+  // Решта коду залишається незмінним
 
   return (
     <div>
