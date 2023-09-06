84ac0e7a80e76271762e00a31740705786a3e68a
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-09-06 09:27:37 +0300

    change-version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// App.js
 import React, { Component } from 'react';
 import { Searchbar } from './SearchBar';
 import { ImageGallery } from './ImageGallery';
@@ -9,69 +8,91 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: [], 
+      images: [],
       searchQuery: '',
       page: 1,
       key: 0,
+      loading: false, 
     };
   }
 
   handleSearch = (query) => {
-    this.setState({
-      images: [], 
-      searchQuery: query,
-      page: 1,
-      key: this.state.key + 1,
-    });
+    this.setState(
+      {
+        images: [],
+        searchQuery: query,
+        page: 1,
+        key: this.state.key + 1,
+        loading: false, 
+      },
+      this.fetchImages
+    );
   };
 
-  loadMoreImages = () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-  };
+  fetchImages = async () => {
+  const { searchQuery, page } = this.state;
+  const url = `https://pixabay.com/api/?key=${API_KEY}&q=${encodeURIComponent(
+    searchQuery
+  )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.totalHits === 0) {
+      console.log('No images found.');
+      return;
+    }
+
+    const newImages = data.hits.map((image) => ({
+      id: image.id,
+      webformatURL: image.webformatURL,
+      tags: image.tags,
+      likes: image.likes,
+      views: image.views,
+      comments: image.comments,
+      downloads: image.downloads,
+    }));
+
+    this.setState((prevState) => ({
+      images: [...prevState.images, ...newImages],
+    }));
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw error; 
+  }
+};
+
+
+  loadMoreImages = async () => {
+  this.setState({
+    page: this.state.page + 1,
+    loading: true, 
+  });
+
+  try {
+    await this.fetchImages();
+  } catch (error) {
+    console.error('Error loading more images:', error);
+  } finally {
+    this.setState({ loading: false });
+  }
+};
+
 
   render() {
-    const { images, searchQuery, page, key } = this.state;
+    const { images, searchQuery, page, key, loading } = this.state;
 
     return (
       <div className="App">
         <Searchbar onSearch={this.handleSearch} />
-        <ImageGallery apiKey={API_KEY} searchQuery={searchQuery} page={page} onLoadMore={this.loadMoreImages} key={key} images={images} />
+        <ImageGallery
+          searchQuery={searchQuery}
+          onLoadMore={this.loadMoreImages}
+          images={images}
+          loading={loading} 
+        />
       </div>
     );
   }
 }
-
-
-
-// import React, { useState } from 'react';
-// import { Searchbar } from './SearchBar';
-// import { ImageGallery } from './ImageGallery';
-
-// const API_KEY = '38400499-9377fca084918dc6c22b9bff8';
-
-// export const App = () => {
-//   const [searchQuery, setSearchQuery] = useState('');
-//   const [page, setPage] = useState(1);
-//   const [key, setKey] = useState(0); 
-
-
-//   const handleSearch = (query) => {
-//     setSearchQuery(query);
-//     setPage(1);
-//     setKey((prevKey) => prevKey + 1);
-//   };
-
-//   const loadMoreImages = () => {
-//     setPage(page + 1);
-//   };
-
-//   return (
-//     <div className="App">
-//       <Searchbar onSearch={handleSearch} />
-      
-//       <ImageGallery apiKey={API_KEY} searchQuery={searchQuery} page={page} onLoadMore={loadMoreImages}   key={key} /> 
-//     </div>
-//   );
-// };
--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -4,84 +4,9 @@ import { Button } from './Button';
 import { Rings } from 'react-loader-spinner';
 
 export class ImageGallery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      images: [],
-      loading: false,
-      totalImages: 0,
-    };
-  }
-
-  componentDidMount() {
-    this.fetchImages();
-  }
-
-  componentDidUpdate(prevProps) {
-    // Перемістіть ваши методи в componentDidUpdate
-    if (
-      prevProps.apiKey !== this.props.apiKey ||
-      prevProps.searchQuery !== this.props.searchQuery ||
-      prevProps.page !== this.props.page ||
-      prevProps.componentKey !== this.props.componentKey
-    ) {
-      this.fetchImages();
-    }
-  }
-
-  fetchImages = async () => {
-    this.setState({ loading: true });
-
-    try {
-      const url = `https://pixabay.com/api/?key=${this.props.apiKey}&q=${encodeURIComponent(
-        this.props.searchQuery
-      )}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.props.page}&per_page=12`;
-
-      const response = await fetch(url);
-      const data = await response.json();
-
-      if (data.totalHits === 0) {
-        console.log('No images found.');
-        return;
-      }
-
-      const newImages = data.hits.map((image) => ({
-        id: image.id,
-        webformatURL: image.webformatURL,
-        tags: image.tags,
-        likes: image.likes,
-        views: image.views,
-        comments: image.comments,
-        downloads: image.downloads,
-      }));
-
-      this.setState((prevState) => ({
-        images: [
-          ...prevState.images,
-          ...newImages.filter(
-            (newImage) =>
-              !prevState.images.some((prevImage) => prevImage.id === newImage.id)
-          ),
-        ],
-        totalImages: data.totalHits,
-      }));
-    } catch (error) {
-      console.error('Error fetching images:', error);
-    } finally {
-      this.setState({ loading: false });
-    }
-  };
-
-  handleLoadMore = () => {
-    this.props.onLoadMore();
-  };
-
   render() {
-    const { images, loading, totalImages } = this.state;
-    const { searchQuery } = this.props;
-if (!searchQuery || images === null) {
-    return null; // Не отображать ничего, если нет запроса или изображений пока нет
-  }
+    const { images, loading, searchQuery } = this.props;
+
     return (
       <div>
         <ul className="ImageGallery">
@@ -103,96 +28,12 @@ if (!searchQuery || images === null) {
             />
           </div>
         )}
-        {images.length > 0 && images.length < totalImages && (
-          <Button onLoadMore={this.handleLoadMore} />
+        {images.length > 0 && (
+          <Button onLoadMore={this.props.onLoadMore} />
         )}
-        {images.length === 0 && searchQuery && <p>No images found.</p>}
+        {images.length === 0 && searchQuery && !loading && <p>No images found.</p>}
+
       </div>
     );
   }
 }
-
-
-// import React, { useEffect, useState } from 'react';
-// import { ImageGalleryItem } from './ImageGalleryItem';
-// import { Button } from './Button';
-// import { Rings } from 'react-loader-spinner';
-
-// export const ImageGallery = ({ apiKey, searchQuery, page, onLoadMore, componentKey }) => {
-//   const [images, setImages] = useState([]);
-//   const [loading, setLoading] = useState(false);
-//   const [totalImages, setTotalImages] = useState(0);
-
-//   useEffect(() => {
-//     const fetchImages = async () => {
-//       setLoading(true);
-
-//       try {
-//         const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(
-//           searchQuery
-//         )}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=12`;
-
-//         const response = await fetch(url);
-//         const data = await response.json();
-
-//         if (data.totalHits === 0) {
-//           console.log('No images found.');
-//           return;
-//         }
-
-//         const newImages = data.hits.map((image) => ({
-//           id: image.id,
-//           webformatURL: image.webformatURL,
-//           tags: image.tags,
-//           likes: image.likes,
-//           views: image.views,
-//           comments: image.comments,
-//           downloads: image.downloads,
-//         }));
-
-//         setImages((prevImages) => [...prevImages, ...newImages.filter(newImage => !prevImages.some(prevImage => prevImage.id === newImage.id))]);
-//         setTotalImages(data.totalHits);
-//       } catch (error) {
-//         console.error('Error fetching images:', error);
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     if (!searchQuery) return;
-//     fetchImages();
-//   }, [apiKey, searchQuery, page, componentKey]);
-
-//   const handleLoadMore = () => {
-//     onLoadMore(); 
-//   };
-
-
-//   return (
-//     <div>
-//       <ul className="ImageGallery">
-//         {images.map((image) => (
-//           <ImageGalleryItem key={image.id} image={image} />
-//         ))}
-//       </ul>
-//       {loading && (
-//         <div className="spinner-container">
-//           <Rings
-//             color="#303f9f"
-//             height="80"
-//             width="80"
-//             radius="6"
-//             wrapperStyle={{}}
-//             wrapperClass=""
-//             visible={true}
-//             ariaLabel="rings-loading"
-//           />
-//         </div>
-//       )}
-//       {images.length > 0 && images.length < totalImages && (
-//         <Button onLoadMore={handleLoadMore} />
-//       )}
-//       {images.length === 0 && searchQuery && <p>No images found.</p>}
-//     </div>
-//   );
-// };
